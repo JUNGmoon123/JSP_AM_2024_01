ab05e93445674e72b3738e2ea6f94fbc15153854
JUNGmoon123 <jungpro567@gmail.com>
2024-01-19 16:31:49 +0900

    8단만 출력하기(스스로)

--- src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
@@ -9,11 +9,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/home/main2")
+@WebServlet("/printDan")
 public class HomeMainServlet2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().append("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!: <br>").append(request.getContextPath()+"<br>");
+		
+		response.getWriter().append("==8단==<br>");
+		
+		for(int i = 2; i<10; i++) {
+			response.getWriter().append("8"+" * "+ i+" = "+8*i+"<br>");
+			response.getWriter().append("\n");
+		}
 	}
 }
