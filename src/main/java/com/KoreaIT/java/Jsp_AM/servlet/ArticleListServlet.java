b84dfdf521fc541a837ed867a87a746a7e4bb7af
JUNGmoon123 <jungpro567@gmail.com>
2024-01-22 15:22:51 +0900

    리팩토링, 메인페이지 추가

--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleListServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleListServlet.java
@@ -1,4 +1,4 @@
-package com.KoreaIT.java.Jsp_AM;
+package com.KoreaIT.java.Jsp_AM.servlet;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -6,6 +6,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+
+import com.KoreaIT.java.Jsp_AM.util.DBUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -26,7 +28,7 @@ public class ArticleListServlet extends HttpServlet {
 			System.out.println("클래스가 없습니다.");
 			e.printStackTrace();
 		}
-	
+
 		String url = "jdbc:mysql://127.0.0.1:3306/JSP_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 		String user = "root";
 		String password = "";
@@ -34,20 +36,16 @@ public class ArticleListServlet extends HttpServlet {
 		Connection conn = null;
 
 		try {
-			conn = DriverManager.getConnection(url, "root", "");
+			conn = DriverManager.getConnection(url, user, password);
 			response.getWriter().append("연결 성공!");
-
-			DBUtil dbUtil = new DBUtil(request, response);
 
 			String sql = "SELECT * FROM article;";
 
-			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
-			
-//			response.getWriter().append(articleRows.toString());
-			//list라는 jsp파일 피요하다.
-			//articleRows를 set으로 넘겨준다.
+			List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
+
 			request.setAttribute("articleRows", articleRows);
 			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
+
 		} catch (SQLException e) {
 			System.out.println("에러 : " + e);
 		} finally {
